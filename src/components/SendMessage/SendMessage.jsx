f507343c57f99f59dca93617d6b271e70268c420
Pavel <dzygman@glincom.com>
2022-10-21 20:21:57 +0300

    add MU

--- src/components/SendMessage/SendMessage.jsx
+++ src/components/SendMessage/SendMessage.jsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Message from '../Message/Message.jsx';
+import TextField from '@mui/material/TextField';
 
 
 
 const SendMessage = (props) => {
 
+    const [focus, setFocus] = useState(true);
+
+    const sendMessage = () => {
+        console.log(focus)
+        setFocus(true)
+        props.addMessage()
+        
+    } 
 
     return (
             <div className='sendmessage'>
-                <input type="text" id="mess" value={props.messageText} onChange={props.writeMessage} ></input>
-                <button onClick={props.addMessage}>Ответить</button>
+                <TextField id="standard-basic" label="Введите сообщение"    focused variant="standard" value={props.messageText} onChange={props.writeMessage}/>
+                <button onClick={sendMessage}>Ответить</button>
             </div>
 
     )
